bd5db79013f3f904cee090caafe4a2e7c2477b34
Joshua Wolff <wolff.joshua2002@icloud.com>
2025-06-06 15:23:17 +0200

    added: player camera movement
    Simply said, the playerController now uses the _unhandled_input function, to detect moouse movement, which will then be used to turn the playerController node.

--- src/playerController.cpp
+++ src/playerController.cpp
@@ -2,8 +2,10 @@
 
 PlayerController::PlayerController() {
     set_physics_process(true);
+    set_process_unhandled_input(true);
 
     speed_ = 10;
+    turnSpeed_ = 1;
 }
 
 PlayerController::~PlayerController() {
@@ -14,12 +16,42 @@ void PlayerController::_bind_methods() {
     ClassDB::bind_method(D_METHOD("getSpeed"), &PlayerController::getSpeed);
     ClassDB::bind_method(D_METHOD("setSpeed", "speed_"), &PlayerController::setSpeed);
 
-    ADD_PROPERTY(PropertyInfo(Variant::FLOAT, "sped"), "setSpeed", "getSpeed");
+    ADD_PROPERTY(PropertyInfo(Variant::FLOAT, "speed"), "setSpeed", "getSpeed");
+
+    ClassDB::bind_method(D_METHOD("getTurnSpeed"), &PlayerController::getTurnSpeed);
+    ClassDB::bind_method(D_METHOD("setTurnSpeed", "turnSpeed_"), &PlayerController::setTurnSpeed);
+
+    ADD_PROPERTY(PropertyInfo(Variant::FLOAT, "turnSpeed"), "setTurnSpeed", "getTurnSpeed");
 }
 
+void PlayerController::_ready() {
+    Input::get_singleton()->set_mouse_mode(Input::MOUSE_MODE_CAPTURED);
+}
+void PlayerController::_input(const Ref<InputEventKey> &event) {
+    if (!event.is_valid())
+        return;
+
+    if (event->is_pressed() && event->get_keycode() == Key::KEY_T) 
+    {
+        pauseControl_ = pauseControl_ ? false : true;
+    }
+}
+void PlayerController::_unhandled_input(const Ref<InputEvent> &event) {
+    Ref<InputEventMouseMotion> mouseMotion = event;
+
+    if (!mouseMotion.is_valid())
+        return;
+
+    Vector2 motion = mouseMotion->get_relative();
+    targetRotation.y -= motion.x * rotationSensitivity.x;
+    targetRotation.x -= motion.y * rotationSensitivity.y;
+
+    targetRotation.x = CLAMP(targetRotation.x, -90.0f, 90.0f);
+}
 void PlayerController::_physics_process(double a_delta) {
     direction_.zero();
     velocity_.zero();
+    aimVelocity_ = 0;
 
     if (Input::get_singleton()->is_key_pressed(KEY_E)) direction_.z -= 1; // Forward
     if (Input::get_singleton()->is_key_pressed(KEY_D)) direction_.z += 1; // Back
@@ -36,9 +68,18 @@ void PlayerController::_physics_process(double a_delta) {
     
     direction_ = to_global(direction_) - get_transform().get_origin();
 
-    velocity_.x = direction_.x * speed_;
-    velocity_.z = direction_.z * speed_;
+    velocity_.x = direction_.x * speed_ * a_delta;
+    velocity_.z = direction_.z * speed_ * a_delta;
 
     set_velocity(velocity_);
     move_and_slide();
+
+
+    if (pauseControl_)
+        return;
+
+    // camera
+    smoothedRotation = smoothedRotation.lerp(targetRotation, (float)a_delta * smoothingSpeed);
+
+    set_rotation_degrees(Vector3(smoothedRotation.x, smoothedRotation.y, 0));
 }
--- src/playerController.hpp
+++ src/playerController.hpp
@@ -5,6 +5,7 @@
 #include <godot_cpp/classes/character_body3d.hpp>
 #include <godot_cpp/classes/input.hpp>
 #include <godot_cpp/classes/input_event_key.hpp>
+#include <godot_cpp/classes/input_event_mouse_motion.hpp>
 #include <godot_cpp/classes/engine.hpp>
 #include <godot_cpp/variant/utility_functions.hpp>
 #include <godot_cpp/classes/camera3d.hpp>
@@ -17,8 +18,18 @@ class PlayerController : public CharacterBody3D {
 
 private:
     float speed_;
+    float turnSpeed_;
+
     Vector3 direction_;
     Vector3 velocity_;
+    float aimVelocity_;
+    
+    Vector2 rotationSensitivity = Vector2(0.1, 0.1);
+    Vector2 smoothedRotation = Vector2();
+    Vector2 targetRotation = Vector2();
+    float smoothingSpeed = 6.0f;
+
+    bool pauseControl_ = false;
 
 protected:
     static void _bind_methods();
@@ -26,10 +37,15 @@ public:
     PlayerController();
     ~PlayerController();
 
+    void _ready();
+    void _input(const Ref<InputEventKey> &event);
+    void _unhandled_input(const Ref<InputEvent> &event);
     void _physics_process(double a_delta);
     
     void setSpeed(const float a_speed) { speed_ = a_speed; };
     float getSpeed() const { return speed_; };
+    void setTurnSpeed(const float a_turnSpeed) { turnSpeed_ = a_turnSpeed; };
+    float getTurnSpeed() const { return turnSpeed_; };
 };
 
 #endif
